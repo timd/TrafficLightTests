907cbab1c822e898d45cf0b64c8e82b046d63f9b
Tim Duckett <tim@charismaticmegafauna.co.uk>
2013-08-29 16:48:30 +0100

    Added UITests placeholders

---
+++ TrafficLightsTests/UITests.m
@@ -0,0 +1,29 @@
+#import "Kiwi.h"
+#import "ViewController.h"
+
+SPEC_BEGIN(UITests)
+
+describe(@"The user interface", ^{
+    
+    context(@"when in the default state", ^{
+        
+        beforeAll(^{
+        });
+        
+        beforeEach(^{
+        });
+        
+        it(@"should show all lights as black", ^{
+        });
+        
+        afterEach(^{
+        });
+        
+        afterAll(^{
+        });
+        
+    });
+    
+});
+
+SPEC_END
