ea458b5c34ec57761aa2f0626b7ef81fa48e10eb
Tim Duckett <tim@charismaticmegafauna.co.uk>
2013-08-29 16:56:38 +0100

    Adds outlets to UITest

--- TrafficLightsTests/UITests.m
+++ TrafficLightsTests/UITests.m
@@ -1,6 +1,19 @@
 #import "Kiwi.h"
 #import "ViewController.h"
 
+@interface ViewController (UITests)
+@property (weak, nonatomic) IBOutlet UIView *upRed;
+@property (weak, nonatomic) IBOutlet UIView *upAmber;
+@property (weak, nonatomic) IBOutlet UIView *upGreen;
+@property (weak, nonatomic) IBOutlet UIView *downRed;
+@property (weak, nonatomic) IBOutlet UIView *downAmber;
+@property (weak, nonatomic) IBOutlet UIView *downGreen;
+@property (weak, nonatomic) IBOutlet UIImageView *stopImageview;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UIButton *tickButton;
+@property (weak, nonatomic) IBOutlet UIButton *stopButton;
+@end
+
 SPEC_BEGIN(UITests)
 
 describe(@"The user interface", ^{
@@ -11,6 +24,7 @@ describe(@"The user interface", ^{
         });
         
         beforeEach(^{
+            
         });
         
         it(@"should show all lights as black", ^{
